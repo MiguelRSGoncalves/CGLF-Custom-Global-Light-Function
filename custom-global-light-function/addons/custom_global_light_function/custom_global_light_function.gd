b4521f78a84f3f326d9265d99989169490421213
Miguel Gonçalves <miguelrsgoncalves@miguelrsgoncalves.com>
2025-08-17 22:57:58 +0100

    Plugin Creation

---
+++ custom-global-light-function/addons/custom_global_light_function/custom_global_light_function.gd
@@ -0,0 +1,12 @@
+@tool
+extends EditorPlugin
+
+var dock : Control
+
+func _enter_tree() -> void:
+	dock = preload("res://addons/custom_global_light_function/cglf.tscn").instantiate()
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, dock)
+
+func _exit_tree() -> void:
+	remove_control_from_docks(dock)
+	dock.free()
