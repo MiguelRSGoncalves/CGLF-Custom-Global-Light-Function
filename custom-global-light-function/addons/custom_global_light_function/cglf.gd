2919ec95510bd1267b1ee6a7b36327d6e467d32b
Miguel Gonçalves <miguelrsgoncalves@miguelrsgoncalves.com>
2025-08-18 14:38:10 +0100

    Added code injection to the shader files

--- custom-global-light-function/addons/custom_global_light_function/cglf.gd
+++ custom-global-light-function/addons/custom_global_light_function/cglf.gd
@@ -1,19 +1,14 @@
 @tool
 extends Control
 
+var _cglf_injection_path: String = "res://addons/custom_global_light_function/cglf.gdshaderinc"
+
+var _cglf_injection_boiler_plate: String = "\n\n//CGLF\n"
+
 func _on_button_pressed() -> void:
 	var shader_files = _find_shader_files("res://")
-	
-	var counter: int = 0
-	for shader_file in shader_files:
-		var shader_file_resource = ResourceLoader.load(shader_file, "Shader", ResourceLoader.CACHE_MODE_REPLACE_DEEP)
-		if shader_file_resource is Shader:
-			counter += 1
-			var code = "HELLO, SHADER! GET RE-SHADED!"
-			shader_file_resource.code += "\n\n" + code
-			ResourceSaver.save(shader_file_resource)
-	print("\nCGLF: Added Custom Global Light Function to ", counter, " shaders!\n")
-	
+	var files_injected: int = _inject_custom_global_light_function(shader_files, _cglf_injection_path)
+	print("\nCGLF: Added Custom Global Light Function to ", files_injected, " shaders!\n")
 
 func _find_shader_files(path: String) -> Array:
 	var results: Array = []
@@ -29,3 +24,13 @@ func _find_shader_files(path: String) -> Array:
 			file_name = dir.get_next()
 		dir.list_dir_end()
 	return results
+
+func _inject_custom_global_light_function(shader_files: Array, code_injection_path: String) -> int:
+	var counter: int = 0
+	for shader_file in shader_files:
+		var shader_file_resource = ResourceLoader.load(shader_file, "Shader", ResourceLoader.CACHE_MODE_REPLACE_DEEP)
+		if shader_file_resource is Shader:
+			counter += 1
+			shader_file_resource.code += _cglf_injection_boiler_plate + '#include "' + code_injection_path + '"'
+			ResourceSaver.save(shader_file_resource, shader_file, ResourceSaver.FLAG_REPLACE_SUBRESOURCE_PATHS)
+	return counter
