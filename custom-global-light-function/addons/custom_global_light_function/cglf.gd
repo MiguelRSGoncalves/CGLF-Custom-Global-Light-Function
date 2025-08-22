e09c29d42bc7859e16d5000ae30bd31a3f4fa7ac
Miguel Gonçalves <miguelrsgoncalves@miguelrsgoncalves.com>
2025-08-22 18:47:18 +0100

    Corrected shader file close after edit

--- custom-global-light-function/addons/custom_global_light_function/cglf.gd
+++ custom-global-light-function/addons/custom_global_light_function/cglf.gd
@@ -3,7 +3,8 @@ extends Control
 
 var _cglf_injection_path: String = "res://addons/custom_global_light_function/cglf.gdshaderinc"
 
-var _cglf_injection_boiler_plate: String = "\n\n//CGLF\n"
+var _cglf_injection_boiler_plate: String = "\n\n//CGLF - Custom Global Light Function\n"
+var _cglf_injection_boiler_plate_ending: String = "\n//CGLF\n"
 
 func _on_button_pressed() -> void:
 	var shader_files = _find_shader_files("res://")
@@ -28,9 +29,19 @@ func _find_shader_files(path: String) -> Array:
 func _inject_custom_global_light_function(shader_files: Array, code_injection_path: String) -> int:
 	var counter: int = 0
 	for shader_file in shader_files:
-		var shader_file_resource = ResourceLoader.load(shader_file, "Shader", ResourceLoader.CACHE_MODE_REPLACE_DEEP)
+		var shader_file_resource = ResourceLoader.load(shader_file, "Shader", ResourceLoader.CACHE_MODE_IGNORE_DEEP)
 		if shader_file_resource is Shader:
 			counter += 1
-			shader_file_resource.code += _cglf_injection_boiler_plate + '#include "' + code_injection_path + '"'
+			shader_file_resource.code += _cglf_injection_boiler_plate + '#include "' + code_injection_path + '"' + _cglf_injection_boiler_plate_ending
 			ResourceSaver.save(shader_file_resource, shader_file, ResourceSaver.FLAG_REPLACE_SUBRESOURCE_PATHS)
+			
+			var fs_dock = EditorInterface.get_file_system_dock()
+			fs_dock.file_removed.emit(shader_file)
+			
+			var fs = EditorInterface.get_resource_filesystem()
+			fs.reimport_files([shader_file])
 	return counter
+
+func _open_cglf_inc():
+	print("OPEN")
+	EditorInterface.edit_script(load(_cglf_injection_path))
